811b0adc2f1ee702cc0a987480624e31196ae044
Parsa Hajiha <hajihaparsa2004@gmail.com>
2023-07-29 14:46:59 -0500

    styles added

--- server/public/map.js
+++ server/public/map.js
@@ -1,16 +1,38 @@
-// map.js
+// Move the initMap function outside the showMap function
 function initMap() {
     var issLocation = {
-        lat: parseFloat(document.getElementById('map').getAttribute('data-lat')),
-        lng: parseFloat(document.getElementById('map').getAttribute('data-long'))
+      lat: parseFloat(document.getElementById('map').getAttribute('data-lat')),
+      lng: parseFloat(document.getElementById('map').getAttribute('data-long'))
     };
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: issLocation,
-        zoom: 4
+      center: issLocation,
+      zoom: 4
     });
     var marker = new google.maps.Marker({
-        position: issLocation,
-        map: map
+      position: issLocation,
+      map: map
     });
 }
   
+function showMap() {
+
+  console.log('Button clicked.');
+  // Remove the "hidden" class to show latitude, longitude, and map
+  const latLongContainer = document.querySelector('.long-lat');
+  const mapContainer = document.querySelector('.google-map');
+  latLongContainer.classList.remove('hidden');
+  mapContainer.classList.remove('hidden');
+
+  // Hide the button after it's clicked
+  const showButton = document.getElementById('showButton');
+  showButton.classList.add('hidden');
+
+  // Call the initMap function when the button is clicked
+  initMap();
+}
+  
+document.addEventListener('DOMContentLoaded', () => {
+   const showButton = document.getElementById('showButton');
+   showButton.addEventListener('click', showMap);
+   console.log('Button element:', showButton);
+});
