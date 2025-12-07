5f16946e2b634fbbae1a9f252da032254fcfe0ae
Parsa Hajiha <hajihaparsa2004@gmail.com>
2025-12-07 17:43:17 -0600

    backend packages updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
+import Tracking from "./pages/Tracking";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/about" element={<About />} />
+        <Route path="/tracking" element={<Tracking />} />
         <Route path="*" element={<div>404 - Not Found</div>} />
       </Routes>
     </Router>
--- server/index.js
+++ server/index.js
@@ -1,32 +1,18 @@
-const express = require('express')
-const path = require('path'); 
-const app = express()
-const axios = require('axios');
-const port = 4000
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 
-require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
 
-const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
-app.get('/', async (req, res) => {
-    try{
-        const response = await axios.get("https://api.wheretheiss.at/v1/satellites/25544");
-        const data = response.data;
-        const lat = data.latitude;
-        const long = data.longitude;
-
-        res.render('main', { GOOGLE_MAPS_API_KEY, lat, long });
-    } catch (error) {
-        console.error("Error fetching data: ", error);
-        res.status(500).send('Error fetching ISS location. Please try again later.');
-    }
+app.get("/", async (req, res) => {
+  res.send("Donation Tracker API is running!");
 });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
-
-app.use(express.static('public'));
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`ISS Locator app listening on port ${port}`);
+});
